d927d40c118d09fd763bd2241dafbf8476691a90
Connor Hitchings <chitchings@pike.com>
2024-09-18 11:08:37 -0400

    Messing with angular and css

--- aloto-app/src/app/app.component.ts
+++ aloto-app/src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [
+    RouterOutlet,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDividerModule
+  ],
 })
 export class AppComponent {
   title = 'aloto-app';
